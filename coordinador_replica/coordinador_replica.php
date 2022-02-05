652208d606dfe18693470b7b7a6b72a9cd426319
levitaxl <hermessamuel10@gmail.com>
2022-02-05 18:00:51 -0400

    ADD- Carga del segundo servidor de replicacion

--- coordinador_replica/coordinador_replica.php
+++ coordinador_replica/coordinador_replica.php
@@ -30,41 +30,83 @@ socket_close($socket);
 
 class ReplicarObjetos{
     public function __invoke($accion,$objetos){
-        //print_r($accion);
-
-        $host    = "localhost";
-        $port    = 20206;
         $data=array();
         $data['accion'] = $accion;
         $data['metodo'] = 'VOTE_REQUEST';
         $data=json_encode($data);
+
+        //SOCKET 1
+        $host    = "localhost";
+        $port    = 20206;
         $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
         $result = socket_connect($socket, $host, $port) or die("Could not connect to server\n");  
         socket_write($socket, $data, strlen($data)) or die("Could not send data to server\n");
         $result = socket_read ($socket, 1024) or die("Could not read server response\n");
-        echo("\nResultado\n");
+        echo("\nResultado Socket 1\n");
         echo($result."\n");
 
-        if($result=='VOTE_COMMIT'){   
-            //Segunda replicacion
-            $response= $this->globalCommit($socket,$objetos);
+        //SOCKET 2
+        $host2    = "localhost";
+        $port2    = 20207;
+        $socket2 = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
+        $result2 = socket_connect($socket2, $host2, $port2) or die("Could not connect to server\n");  
+        socket_write($socket2, $data, strlen($data)) or die("Could not send data to server\n");
+        $result2 = socket_read ($socket2, 1024) or die("Could not read server response\n");
+        echo("\nResultado Socket 2\n");
+        echo($result2."\n");
+        
+        //Las 2 replicas estuvieron bien
 
+
+        if($result=='VOTE_COMMIT' && $result2=='VOTE_COMMIT' ){   
+            $response= $this->globalCommit($socket,$socket2,$objetos);
+        }
+        else if($result=='VOTE_ABORT'){
+            echo('VOTE_ABORT del primer servidor');
+            $this->globalAbort($socket,$socket2);
+            $response='false';
+        }
+
+        else if($result2=='VOTE_ABORT'){
+            echo('VOTE_ABORT del segundo servidor');
+            $this->globalAbort($socket,$socket2);
+            $response='false';
         }
         socket_close($socket);
+        socket_close($socket2);
+
+        echo("\RESPUESTA\n");
+        echo($response);
         return $response;
     }
 
-    private function  globalCommit($socket,$objetos){
+    private function  globalCommit($socket,$socket2,$objetos){
         $data=array();
         $data['objetos'] = $objetos;
         $data['metodo'] = 'GLOBAL_COMMIT';
         $data=json_encode($data);
         socket_write($socket, $data, strlen($data)) or die("Could not send data to server\n");
         $result = socket_read ($socket, 1024) or die("Could not read server response\n");
-        echo("\nResultado Del Global Commit\n");
+        socket_write($socket2, $data, strlen($data)) or die("Could not send data to server\n");
+        $result2 = socket_read ($socket2, 1024) or die("Could not read server response\n");
+        echo("\Global Commit Exitoso\n");
+
+        return true;
+    }
+
+    private function globalAbort($socket,$socket2){
+        $data=array();
+        $data['metodo'] = 'GLOBAL_ABORT';
+        $data=json_encode($data);
+        socket_write($socket, $data, strlen($data)) or die("Could not send data to server\n");
+        $result = socket_read ($socket, 1024) or die("Could not read server response\n");
+        socket_write($socket2, $data, strlen($data)) or die("Could not send data to server\n");
+        $result2 = socket_read ($socket2, 1024) or die("Could not read server response\n");
+        echo("\nResultado Del Global Abort\n");
         echo($result."\n");
 
         return $result;
+
     }
 }
 
--- servidor_replica/server_replica2.php
+++ servidor_replica/server_replica2.php
@@ -9,19 +9,45 @@ $result = socket_bind($socket, $host, $port) or die("Could not bind to socket\n"
 echo('server is running');
 $result = socket_listen($socket, 3) or die("Could not set up socket listener\n");
 do{
-    $spawn = socket_accept($socket) or die("Could not accept incoming connection\n");
-    $data = socket_read($spawn, 1024) or die("Could not read input\n");
-    $data=json_decode($data);
+    $spawn  = socket_accept($socket) or die("Could not accept incoming connection\n");
+    $data   = socket_read($spawn, 1024) or die("Could not read input\n");
+    $data   =json_decode($data);
+    print_r($data);
     $accion=$data->accion;
     $metodo=$data->metodo;
     if($metodo=='VOTE_REQUEST'){
         $output='';
         if($accion=='COMMIT')       $output='VOTE_COMMIT';
-        else if($acccion=='ABORT')  $output='VOTE_ABORT';    
+        else if($accion=='ABORT')  $output='VOTE_ABORT';    
     }
-    
-   
+
     socket_write($spawn, $output, strlen ($output)) or die("Could not write output\n");
+
+    $data = socket_read($spawn, 1024) or die("Could not read input\n");
+    $data=json_decode($data);
+
+    $output=false;
+    if($data->metodo=="GLOBAL_COMMIT"){
+        print_r("GLOBAL COMMIT\n");
+        print_r($data);
+
+        $objetos=$data->objetos;
+        $bytes = file_put_contents("myfile2.json", $objetos); 
+
+        $output=true;
+    }
+
+    else if($data->metodo=='GLOBAL_ABORT'){
+        print_r("GLOBAL ABORT\n");
+        $output='false';
+    }
+
+    print_r("GLOBAL RESPONSE\n");
+    print_r($output);
+    socket_write($spawn, $output, strlen ($output)) or die("Could not write output\n");
+
+
+   
 }while(true);
 
 
